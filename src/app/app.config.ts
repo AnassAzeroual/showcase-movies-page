fb49803a5a9430449f3c8628f31f6da1d13ca52c
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-07-10 01:32:11 +0100

    "Updated package.json and app.config.ts files"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideNzIcons } from './icons-provider';
-import { provideImgixLoader, registerLocaleData } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import fr from '@angular/common/locales/fr';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from '../shared/services/token-interceptor.service';
+import { routes } from './app.routes';
+import { provideNzIcons } from './icons-provider';
 
 registerLocaleData(fr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideNzIcons(),
     importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
